c5a0b97e4bf71c9d429836785bbaad5112ad3943
Tim Davis <tim.davis.instinct@gmail.com>
2021-06-24 08:03:23 -0500

    Update variables.tf

--- examples/kubernetes/basic-cluster/variables.tf
+++ examples/kubernetes/basic-cluster/variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   description = "A prefix used for all resources in this example"
+  default = "vtimd-"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
+  default = "South Central US"
 }
